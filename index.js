1d0a37ea2851d381b1f136659fadab0ba5d38dd6
Victor Gusev <viktor@nmmlm.com>
2019-02-08 22:10:17 +0200

    Code style

--- index.js
+++ index.js
@@ -3,23 +3,22 @@ const isNumber = require('is-number');
 
 /**
  * @param {Array} arr
+ * @returns {*}
  */
 function sort(arr) {
-  if (!isArray(arr))
-    return false;
-  
-  let max = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    const element = arr[i];
+    if (!isArray(arr))
+        return false;
 
-    if (!isNumber(element) || element <= max) {
-      arr.splice(i--, 1);
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        const element = arr[i];
+
+        if (!isNumber(element) || element <= max)
+            arr.splice(i--, 1);
+        else if (element > max)
+            max = element;
     }
-    else if (element > max) {
-      max = element;
-    }
-  }
-  return arr;
+    return arr;
 }
 
 module.exports = sort;
